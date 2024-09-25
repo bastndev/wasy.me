c2f17e86910e2e0fcaf8f92113095d1ddbeb46c4
bastndev <gohit24@gmail.com>
2024-09-25 15:20:46 -0500

    Refactor component styles to remove unnecessary background gradient and adjust size

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -1,19 +1,13 @@
 export const Background = {
     Main: () => (
-        <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-black bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-            <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-gray-400 dark:bg-gray-500/75 opacity-20 blur-[100px]"></div>
-        </div>
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-gray-400 dark:bg-gray-500/75 opacity-20 blur-[100px]"></div>
     ),
     // -- --- --- -- Tech
     Flutter: () => (
-        <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-black bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-            <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-sky-400 dark:bg-sky-500/75 opacity-20 blur-[100px]"></div>
-        </div>
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-sky-400 dark:bg-sky-500/75 opacity-20 blur-[100px]"></div>
     ),
     Astro: () => (
-        <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-black bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-            <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-orange-400 dark:bg-orange-500/75 opacity-20 blur-[100px]"></div>
-        </div>
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-orange-400 dark:bg-orange-500/75 opacity-20 blur-[100px]"></div>
     ),
 
 };
