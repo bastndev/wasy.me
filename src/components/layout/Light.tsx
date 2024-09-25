14e6f0d48520c6b39634cbc39dcd7b5cbf411970
bastndev <gohit24@gmail.com>
2024-09-25 15:44:47 -0500

    Refactor component names and imports to use Light instead of Background

--- src/components/layout/Light.tsx
+++ src/components/layout/Light.tsx
@@ -1,4 +1,4 @@
-export const Background = {
+export const Light = {
     Main: () => (
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-gray-400 dark:bg-gray-500/75 opacity-20 blur-[100px]"></div>
     ),
