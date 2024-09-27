68e0e449b85aa3a3cb5248eeb959141cc2ac1db7
bastndev <gohit24@gmail.com>
2024-09-26 23:53:30 -0500

    [Refactor]  and delete archives

--- src/icon/icons.tsx
+++ src/icon/icons.tsx
@@ -1,4 +1,7 @@
+
 export const Icons = {
+
+    // --- --- --- --  Tech
     GitHub: ({ ...props }) => (
         <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" {...props}>
             <path
@@ -27,21 +30,7 @@ export const Icons = {
             <path d="M9 18c0 1.5 2 3 3 4c1 -1 3 -3 3 -4q -3 1.5 -6 0" />
         </svg>
     ),
-    // -- --- --- Search 
-    Ghost: ({ ...props }) => (
-        <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            aria-hidden="true"
-            {...props}
-        >
-            <title>Ghost</title>
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 1.999l.041 .002l.208 .003a8 8 0 0 1 7.747 7.747l.003 .248l.177 .006a3 3 0 0 1 2.819 2.819l.005 .176a3 3 0 0 1 -3 3l-.001 1.696l1.833 2.75a1 1 0 0 1 -.72 1.548l-.112 .006h-10c-3.445 .002 -6.327 -2.49 -6.901 -5.824l-.028 -.178l-.071 .001a3 3 0 0 1 -2.995 -2.824l-.005 -.175a3 3 0 0 1 3 -3l.004 -.25a8 8 0 0 1 7.996 -7.75zm0 10.001a2 2 0 0 0 -2 2a1 1 0 0 0 1 1h2a1 1 0 0 0 1 -1a2 2 0 0 0 -2 -2zm-1.99 -4l-.127 .007a1 1 0 0 0 .117 1.993l.127 -.007a1 1 0 0 0 -.117 -1.993zm4 0l-.127 .007a1 1 0 0 0 .117 1.993l.127 -.007a1 1 0 0 0 -.117 -1.993z" />
-        </svg>
-    ),
+    // --- --- --- -- Search 
     Search: ({ ...props }) => (
         <svg
             width="24"
@@ -52,8 +41,10 @@ export const Icons = {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+            // className="icon icon-tabler icons-tabler-outline icon-tabler-search"
             aria-hidden="true"
+            // id={props.id}
+            // className={props.class}
             {...props}
         >
             <title>Search</title>
