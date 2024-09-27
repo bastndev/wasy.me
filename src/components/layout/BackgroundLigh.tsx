8fe182052b07207793b210952d1c22513c291ab4
bastndev <gohit24@gmail.com>
2024-09-27 13:45:04 -0500

    [better] code layout--Component

--- src/components/layout/BackgroundLigh.tsx
+++ src/components/layout/BackgroundLigh.tsx
@@ -1,13 +1,11 @@
 export const BgLight = {
-    Grey: () => (
-        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-gray-400 dark:bg-gray-500/75 opacity-20 blur-[100px]"></div>
-    ),
-    // -- --- --- -- Tech
     Blue: () => (
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-sky-400 dark:bg-sky-500/75 opacity-20 blur-[100px]"></div>
+    ),
+    Grey: () => (
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-gray-400 dark:bg-gray-500/75 opacity-20 blur-[100px]"></div>
     ),
     Orange: () => (
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-orange-400 dark:bg-orange-500/75 opacity-20 blur-[100px]"></div>
     ),
-
 };
