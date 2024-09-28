0b1cbf54c81a8d7a6c1716b4813557fa6f8c4fbe
bastndev <gohit24@gmail.com>
2024-09-28 17:03:53 -0500

    Refactor Card component to adjust height in Card.astro

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -5,40 +5,11 @@ export const ALL: SpeakerProps[] = [
     img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
   },
   {
-    name: "Table Icon",
+    name: "Gohit Bastian",
     link: "https://github.com/bastndev",
     img: "https://avatars.githubusercontent.com/u/113950039?v=4",
   },
-  {
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
-  },
-  {
-    name: "Table Icon",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
-  },
-  {
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
-  },
-  {
-    name: "Table Icon",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
-  },
-  {
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
-  },
-  {
-    name: "Table Icon",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
-  },
+ 
 ];
 
 export interface SpeakerProps {
