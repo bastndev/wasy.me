c9c7afaf5e16e9a760fd78d31e51584cd632a57d
bastndev <gohit24@gmail.com>
2024-09-28 18:27:39 -0500

    Refactor Card component to add animation delays

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -19,7 +19,7 @@ export const ALL: AllTechIndex[] = [
     link: "https://github.com/bastndev",
     img: "https://avatars.githubusercontent.com/u/113950039?v=4",
   },
- 
+
   {
     name: "Table Icon",
     link: "https://tabler.io/icons",
@@ -30,8 +30,26 @@ export const ALL: AllTechIndex[] = [
     link: "https://github.com/bastndev",
     img: "https://avatars.githubusercontent.com/u/113950039?v=4",
   },
- 
- 
+  {
+    name: "Table Icon",
+    link: "https://tabler.io/icons",
+    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
+  },
+  {
+    name: "Gohit Bastian",
+    link: "https://github.com/bastndev",
+    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
+  },
+  {
+    name: "Table Icon",
+    link: "https://tabler.io/icons",
+    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
+  },
+  {
+    name: "Gohit Bastian",
+    link: "https://github.com/bastndev",
+    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
+  },
 ];
 
 export interface AllTechIndex {
