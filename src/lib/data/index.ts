1f86ceebd3ef3c247e1f3197da819049c93b6a5a
bastndev <gohit24@gmail.com>
2024-09-28 17:28:18 -0500

    🃏 [Refactor] Card component to adjust height and improve layout

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,4 +1,14 @@
-export const ALL: SpeakerProps[] = [
+export const ALL: AllTechIndex[] = [
+  {
+    name: "Table Icon",
+    link: "https://tabler.io/icons",
+    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
+  },
+  {
+    name: "Gohit Bastian",
+    link: "https://github.com/bastndev",
+    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
+  },
   {
     name: "Table Icon",
     link: "https://tabler.io/icons",
@@ -10,9 +20,21 @@ export const ALL: SpeakerProps[] = [
     img: "https://avatars.githubusercontent.com/u/113950039?v=4",
   },
  
+  {
+    name: "Table Icon",
+    link: "https://tabler.io/icons",
+    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
+  },
+  {
+    name: "Gohit Bastian",
+    link: "https://github.com/bastndev",
+    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
+  },
+ 
+ 
 ];
 
-export interface SpeakerProps {
+export interface AllTechIndex {
   name: string;
   img: string;
   link: string;
