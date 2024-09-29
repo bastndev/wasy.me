58a91e985015cecf0f3f1a6e2d70eb2ce7399df0
bastndev <gohit24@gmail.com>
2024-09-29 00:06:32 -0500

    [add] Data flutter to project and better and add CardFlutter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  output: 'server',
+  integrations: [react(), tailwind()],
 });
---
+++ src/lib/data/flutter/data.ts
@@ -0,0 +1,121 @@
+export const FLUTTER: FlutterTech[] = [
+  // MARK: --- -- -- ALL
+  {
+    id: "all",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://avatars.githubusercontent.com/u/14985020?s=200&v=4",
+  },
+  {
+    id: "all",
+    name: "Miguel Angel",
+    link: "https://github.com/midudev",
+    img: "https://avatars.githubusercontent.com/u/1561955?v=4",
+  },
+
+  // MARK: --- -- -- API
+  {
+    id: "api",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/78917614?v=4",
+  },
+  {
+    id: "api",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/78917614?v=4",
+  },
+  {
+    id: "api",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/78917614?v=4",
+  },
+
+  // MARK: --- -- -- ICONS
+  {
+    id: "icons",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "icons",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- VSCODE
+  {
+    id: "vscode",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "vscode",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "vscode",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- WEB
+  {
+    id: "web",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "web",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "web",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- COURSES
+  {
+    id: "courses",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "courses",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "courses",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- FONTS
+  {
+    id: "fonts",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+
+];
+
+export interface FlutterTech {
+  id: string;
+  name: string;
+  img: string;
+  link: string;
+}
