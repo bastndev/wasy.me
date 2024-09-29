7fd7bf88058d781c4dcee732afbcf6150320ff58
bastndev <gohit24@gmail.com>
2024-09-29 00:20:57 -0500

    [add] Data astroJs and add CardAstro

---
+++ src/lib/data/astro/data.ts
@@ -0,0 +1,120 @@
+export const ASTROJS: AstroTech[] = [
+  // MARK: --- -- -- ALL
+  {
+    id: "all",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.pixabay.com/photo/2023/08/20/23/15/grasshopper-8203319_1280.jpg",
+  },
+  {
+    id: "all",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.pixabay.com/photo/2023/08/20/23/15/grasshopper-8203319_1280.jpg",
+  },
+
+  // MARK: --- -- -- API
+  {
+    id: "api",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/78917614?v=4",
+  },
+  {
+    id: "api",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/78917614?v=4",
+  },
+  {
+    id: "api",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/78917614?v=4",
+  },
+
+  // MARK: --- -- -- ICONS
+  {
+    id: "icons",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "icons",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- VSCODE
+  {
+    id: "vscode",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "vscode",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "vscode",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- WEB
+  {
+    id: "web",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "web",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "web",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- COURSES
+  {
+    id: "courses",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "courses",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  {
+    id: "courses",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+  // MARK: --- -- -- FONTS
+  {
+    id: "fonts",
+    name: "Lucas Mantilla",
+    link: "https://www.youtube.com/watch?v=WRc8lz-bp78&t=9154s",
+    img: "https://avatars.githubusercontent.com/u/112582162?v=4",
+  },
+];
+
+export interface AstroTech {
+  id: string;
+  name: string;
+  img: string;
+  link: string;
+}
