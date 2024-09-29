4c32305027405ba1d3ee9dcfe87253a471ee4ac8
bastndev <gohit24@gmail.com>
2024-09-29 00:51:36 -0500

    Refactor Card component to filter tech by id

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,58 +1,59 @@
 export const ALL: AllTechIndex[] = [
+  // MARK: --- -- -- Index home
   {
+    id: "home",
     name: "Table Icon",
     link: "https://tabler.io/icons",
     img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
   },
   {
+    id: "home",
     name: "Gohit Bastian",
     link: "https://github.com/bastndev",
     img: "https://avatars.githubusercontent.com/u/113950039?v=4",
   },
   {
+    id: "home",
     name: "Table Icon",
     link: "https://tabler.io/icons",
     img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
   },
+  // MARK: --- -- -- Index Flutter
   {
-    name: "Gohit Bastian",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
-  },
-
-  {
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
+    id: "fluter",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.pixabay.com/photo/2023/08/20/23/15/grasshopper-8203319_1280.jpg",
   },
   {
-    name: "Gohit Bastian",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
+    id: "flutter",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.pixabay.com/photo/2023/08/20/23/15/grasshopper-8203319_1280.jpg",
+  },
+  // MARK: --- -- -- Index Astro
+  {
+    id: "astro",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.worldvectorlogo.com/logos/astrojs.svg",
   },
   {
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
+    id: "astro",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.worldvectorlogo.com/logos/astrojs.svg",
   },
   {
-    name: "Gohit Bastian",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
-  },
-  {
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
-  },
-  {
-    name: "Gohit Bastian",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
+    id: "astro",
+    name: "Vercel",
+    link: "https://github.com/vercel",
+    img: "https://cdn.worldvectorlogo.com/logos/astrojs.svg",
   },
 ];
 
 export interface AllTechIndex {
+  id: string;
   name: string;
   img: string;
   link: string;
---
+++ src/lib/versions.ts
@@ -0,0 +1,22 @@
+export const VERSIONS: VersionTech[] = [
+  // MARK: --- -- -- wasy.me
+  {
+    id: "wasy",
+    version: "v0.0.5",
+  },
+  // MARK: --- -- -- Flutter
+  {
+    id: "flutter",
+    version: "3.24",
+  },
+  // MARK: --- -- -- AstroJs
+  {
+    id: "astro",
+    version: "v5.0 Beta",
+  },
+];
+
+export interface VersionTech {
+  id: string;
+  version: string;
+}
