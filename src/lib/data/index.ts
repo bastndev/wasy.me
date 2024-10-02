6b30efcd7a1c8956792ab73afb2f246d3a3c70a2
bastndev <gohit24@gmail.com>
2024-10-02 00:52:41 -0500

    random box img

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,23 +1,4 @@
 export const ALL: AllTechIndex[] = [
-  // MARK: --- -- -- Index home
-  {
-    id: "home",
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
-  },
-  {
-    id: "home",
-    name: "Gohit Bastian",
-    link: "https://github.com/bastndev",
-    img: "https://avatars.githubusercontent.com/u/113950039?v=4",
-  },
-  {
-    id: "home",
-    name: "Table Icon",
-    link: "https://tabler.io/icons",
-    img: "https://avatars.githubusercontent.com/u/35471246?s=200&v=4",
-  },
   // MARK: --- -- -- Index Flutter
   {
     id: "flutter",
