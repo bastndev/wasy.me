69fa1a658f65a6cf6aed9cbfdde6aa762ae8f64e
bastndev <gohit24@gmail.com>
2024-10-04 02:01:56 -0500

    [better] background search

--- src/components/layout/BackgroundLigh.tsx
+++ src/components/layout/BackgroundLigh.tsx
@@ -8,4 +8,7 @@ export const BgLight = {
     Orange: () => (
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-orange-400 dark:bg-orange-500/75 opacity-20 blur-[100px]"></div>
     ),
+    White: () => (
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-white dark:bg-white/80 opacity-20 blur-[100px]"></div>
+    ),
 };
