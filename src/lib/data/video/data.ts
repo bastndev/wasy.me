998a693a1682bfeb6976d5b91b1696be2f448519
bastndev <gohit24@gmail.com>
2025-03-29 21:00:19 -0500

    [UPDATE] Revise video links and enhance descriptions for better clarity

--- src/lib/data/video/data.ts
+++ src/lib/data/video/data.ts
@@ -1,197 +1,226 @@
 export const VIDEO: FlutterTech[] = [
-  // MARK: --- -- -- USE
+  // MARK: --- -- -- Material
   {
-    id: "use",
-    name: "YT Video DW",
-    img: "https://iili.io/3uYeNAF.jpg",
-    link: ""
-  }, {
-    id: "use",
-    name: "YT Video DW",
-    img: "https://iili.io/3uaz1a9.webp",
-    link: "https://y2mate.nu/en-ClIf/"
-  }, {
-    id: "use",
-    name: "Moises",
-    img: "https://iili.io/3uanJ7s.webp",
-    link: "https://studio.moises.ai/library/"
-  }, {
-    id: "use",
-    name: "Text Video",
-    img: "https://iili.io/3uaA2Fp.jpg",
-    link: " https://clip.opus.pro/captions"
-  }, {
-    id: "use",
-    name: "Remove Background",
-    img: "https://iili.io/3ua5YCb.webp",
-    link: "https://www.pixelcut.ai/es/quitar-fondo-imagen"
-  }, {
-    id: "use",
-    name: "Delete Object",
-    img: "https://iili.io/3ua05ZP.webp",
-    link: "https://www.photoroom.com/tools/remove-object-from-photoK "
-  }, {
-    id: "use",
-    name: "Jitter",
-    img: "https://iili.io/3uavpb1.png",
-    link: "https://jitter.video/"
-  }, {
-    id: "use",
-    name: "Rotato",
-    img: "https://iili.io/3uarh6g.png",
-    link: "https://rotato.app/"
-  }, {
-    id: "use",
-    name: "Shorts",
-    img: "https://iili.io/3uaPcas.jpg",
-    link: "https://shots.so/"
-  }, {
-    id: "use",
-    name: "Back IMG",
-    img: "https://iili.io/3uas311.webp",
-    link: " https://pngtree.com/"
-  }, {
-    id: "use",
-    name: "Video",
-    img: "https://iili.io/3uaDXbs.webp",
-    link: "https://klingai.com/global/"
-  }, {
-    id: "use",
-    name: "Udio",
-    img: "https://iili.io/3uapYzu.png",
-    link: "https://www.udio.com/"
-  }, {
-    id: "use",
-    name: "3D",
-    img: "https://iili.io/3uc9wKv.jpg",
-    link: "https://app.spline.design/home "
-  }, {
-    id: "use",
-    name: "Photoshop cloud",
-    img: "https://iili.io/3ucKiQI.webp",
-    link: "https://photoshop.adobe.com/"
-  }, {
-    id: "use",
-    name: "Photopea",
-    img: "https://iili.io/3ucui1R.webp",
-    link: "https://www.photopea.com/ "
-  },
-  // -- --- USE ME
-  {
-    id: "use",
-    name: "Voice",
-    img: "https://iili.io/3ucRXKx.png",
-    link: "https://podcast.adobe.com/enhance  "
-  }, {
-    id: "use",
-    name: "Pexel Video",
-    img: "https://iili.io/3ulrnyv.webp",
-    link: "https://www.pexels.com/videos/"
-  }, {
-    id: "use",
-    name: "vidnoz.com",
-    img: "https://iili.io/3ul6TrX.webp",
-    link: "https://www.vidnoz.com/voice-changer.html "
-  }, {
-    id: "use",
-    name: "myinstants",
-    img: "https://iili.io/3ulPyKP.jpg",
-    link: "https://www.myinstants.com/es/categories/Sound%20Effects/ "
-  }, {
-    id: "use",
-    name: "ni3.app banner",
-    img: "https://iili.io/3ulQLrB.webp",
-    link: "https://www.ni3.app/community"
-  }, {
-    id: "use",
-    name: "translate.yandex",
-    img: "https://iili.io/3ulbdEx.webp",
-    link: "https://translate.yandex.com/en/ocr?"
-  }, {
-    id: "use",
-    name: "upscale.media",
-    img: "https://iili.io/3ulywuI.webp",
-    link: "https://www.upscale.media/es"
-  }, {
-    id: "use",
+    id: "zeta",
     name: "aistudio",
     img: "https://iili.io/3u0Kddu.webp",
     link: "https://aistudio.google.com/prompts/new_chat"
   }, {
-    id: "use",
-    name: "remove Logo",
-    img: "https://iili.io/3u0f7yb.webp",
-    link: "https://www.remove.bg/es/upload"
-  }, {
-    id: "use",
-    name: "browserling",
-    img: "https://iili.io/3u0qOJe.webp",
-    link: "https://www.browserling.com/"
-  }, {
-    id: "use",
-    name: "usepastel",
-    img: "https://iili.io/3u0BcBV.webp",
-    link: "https://usepastel.com/responsive-website-test"
-  }, {
-    id: "use",
+    id: "zeta",
     name: "testingbot",
     img: "https://iili.io/3u0xNNn.webp",
     link: "https://testingbot.com/members"
   }, {
-    id: "use",
+    id: "zeta",
+    name: "browserling",
+    img: "https://iili.io/3u0qOJe.webp",
+    link: "https://www.browserling.com/"
+  }, {
+    id: "zeta",
+    name: "usepastel",
+    img: "https://iili.io/3u0BcBV.webp",
+    link: "https://usepastel.com/responsive-website-test"
+  }, {
+    id: "material",
+    name: "Download MP3 🎵",
+    img: "https://iili.io/3uaz1a9.webp",
+    link: "https://y2mate.nu/en-ClIf/"
+  }, {
+    id: "material",
+    name: "Download MP4 🎥",
+    img: "https://iili.io/3uYeNAF.jpg",
+    link: "https://www.socialplug.io/free-tools/youtube-video-downloader"
+  }, {
+    id: "material",
+    name: "Download MP4-2",
+    img: "https://iili.io/3AITBGR.webp",
+    link: "https://ytmp3.so/en/youtube-4k-downloader"
+  }, {
+    id: "material",
+    name: "Download MP3-2",
+    img: "https://iili.io/3AT4PbR.jpg",
+    link: "https://www.hirequotient.com/youtube-to-mp3"
+  }, {
+    id: "material",
+    name: "DW MP3-MP4",
+    img: "https://iili.io/3ATPS9a.webp",
+    link: "https://ytmp3.sh/es43/?utm_source=chatgpt.com/"
+  }, {
+    id: "material",
+    name: "bank-img",
+    img: "https://iili.io/3AIA9mF.webp",
+    link: "https://freeimage.host/"
+  },
+  // MARK: --- -- -- IMAGE
+  {
+    id: "image",
+    name: "Remove Background",
+    img: "https://iili.io/3ua5YCb.webp",
+    link: "https://www.pixelcut.ai/es/quitar-fondo-imagen"
+  }, {
+    id: "image",
+    name: "Delete Object",
+    img: "https://iili.io/3ua05ZP.webp",
+    link: "https://www.photoroom.com/tools/"
+  }, {
+    id: "image",
+    name: "Shorts",
+    img: "https://iili.io/3uaPcas.jpg",
+    link: "https://shots.so/"
+  }, {
+    id: "image",
+    name: "Back IMG",
+    img: "https://iili.io/3uas311.webp",
+    link: " https://pngtree.com/"
+  }, {
+    id: "image",
+    name: "Photoshop cloud",
+    img: "https://iili.io/3ucKiQI.webp",
+    link: "https://photoshop.adobe.com/"
+  }, {
+    id: "image",
+    name: "Photopea",
+    img: "https://iili.io/3ucui1R.webp",
+    link: "https://www.photopea.com/ "
+  }, {
+    id: "image",
+    name: "translate.yandex",
+    img: "https://iili.io/3ulbdEx.webp",
+    link: "https://translate.yandex.com/en/ocr?"
+  }, {
+    id: "image",
+    name: "remove Logo",
+    img: "https://iili.io/3u0f7yb.webp",
+    link: "https://www.remove.bg/es/upload"
+  }, {
+    id: "image",
+    name: "myedit.online",
+    img: "https://iili.io/3u1MM79.webp",
+    link: "https://myedit.online/es/photo-editor/ai-replace?utm_source=blog_esp&utm_medium=referral&utm_campaign=3009-quitar-la-ropa-en-una-foto&affid=2581_-1_1005_3009_quitar-la-ropa-en-una-foto"
+  }, {
+    id: "image",
+    name: "upscale.media",
+    img: "https://iili.io/3ulywuI.webp",
+    link: "https://www.upscale.media/es"
+  }, {
+    id: "image",
+    name: "ni3.app banner",
+    img: "https://iili.io/3ulQLrB.webp",
+    link: "https://www.ni3.app/community"
+  }, {
+    id: "image",
+    name: "piclumen",
+    img: "https://iili.io/3u0lQxp.webp",
+    link: "https://www.piclumen.com/"
+  },
+  // MARK: --- -- -- VIDEO
+  {
+    id: "video",
+    name: "Text Video",
+    img: "https://iili.io/3uaA2Fp.jpg",
+    link: " https://clip.opus.pro/captions"
+  }, {
+    id: "video",
+    name: "Video",
+    img: "https://iili.io/3uaDXbs.webp",
+    link: "https://klingai.com/global/"
+  }, {
+    id: "video",
+    name: "Pexel Video",
+    img: "https://iili.io/3ulrnyv.webp",
+    link: "https://www.pexels.com/videos/"
+  }, {
+    id: "video",
     name: "yarn.co",
     img: "https://iili.io/3u0IxrQ.webp",
     link: "https://yarn.co/"
   }, {
-    id: "use",
+    id: "video",
     name: "clip.cafe",
     img: "https://iili.io/3u0TcN9.webp",
     link: "https://clip.cafe/12-monkeys-2015/she-meant-us/"
   }, {
-    id: "use",
+    id: "video",
     name: "playphrase.me",
     img: "https://iili.io/3u0A3kN.webp",
     link: "https://www.playphrase.me/#/search?q=fuck+up&pos=0"
   }, {
-    id: "use",
+    id: "video",
     name: "cinepacks.store",
     img: "https://iili.io/3u0R1oX.webp",
     link: "https://cinepacks.store/?srsltid=AfmBOorevigF56DA85m2798vF4RXen1YeumxDbfzuJbZ1tkbkPVjfPSx"
   }, {
-    id: "use",
+    id: "video",
     name: "dreamina.capcut",
     img: "https://iili.io/3u0a1e4.webp",
     link: "https://dreamina.capcut.com/"
   }, {
-    id: "use",
-    name: "piclumen",
-    img: "https://iili.io/3u0lQxp.webp",
-    link: "https://www.piclumen.com/"
-  }, {
-    id: "use",
-    name: "upscale",
-    img: "https://iili.io/3u00gQ2.webp",
-    link: "https://www.upscale.media/es"
-  }, {
-    id: "use",
-    name: "wallpaperengin",
-    img: "https://iili.io/3u0E2PS.webp",
-    link: "Wallaper | https://www.wallpaperengine.space/collections/best-in-4k"
-  }, {
-    id: "use",
+    id: "video",
     name: "capcut",
     img: "https://iili.io/3u0MFyu.webp",
     link: "https://www.capcut.com/"
   }, {
-    id: "use",
+    id: "video",
     name: "clipchamp",
     img: "https://iili.io/3u0MsMQ.webp",
     link: "https://clipchamp.com/"
+  },
+  // MARK:--- -- --  SOUND
+  {
+    id: "sound",
+    name: "vidnoz.com",
+    img: "https://iili.io/3ul6TrX.webp",
+    link: "https://www.vidnoz.com/voice-changer.html "
   }, {
-    id: "use",
+    id: "sound",
+    name: "Moises",
+    img: "https://iili.io/3uanJ7s.webp",
+    link: "https://studio.moises.ai/library/"
+  }, {
+    id: "sound",
+    name: "Udio",
+    img: "https://iili.io/3uapYzu.png",
+    link: "https://www.udio.com/"
+  }, {
+    id: "audio",
+    name: "Voice",
+    img: "https://iili.io/3ucRXKx.png",
+    link: "https://podcast.adobe.com/enhance  "
+  }, {
+    id: "sound",
+    name: "myinstants",
+    img: "https://iili.io/3ulPyKP.jpg",
+    link: "https://www.myinstants.com/es/categories/Sound%20Effects/ "
+  },
+  // MARK:--- -- --  EFFECT
+  {
+    id: "effect",
+    name: "Jitter",
+    img: "https://iili.io/3uavpb1.png",
+    link: "https://jitter.video/"
+  }, {
+    id: "effect",
+    name: "Rotato",
+    img: "https://iili.io/3uarh6g.png",
+    link: "https://rotato.app/"
+  }, {
+    id: "effect",
+    name: "3D",
+    img: "https://iili.io/3uc9wKv.jpg",
+    link: "https://app.spline.design/home "
+  }, {
+    id: "effect",
     name: "alightmotion",
     img: "https://iili.io/3u1WBFp.webp",
     link: "https://guide.alightmotion.com/effects/"
+  },
+
+  // MARK: --- -- -- USE
+  {
+    id: "image",
+    name: "wallpaperengin",
+    img: "https://iili.io/3u0E2PS.webp",
+    link: "https://www.wallpaperengine.space/collections/best-in-4k"
   }, {
     id: "use",
     name: "ytsavepro",
@@ -227,11 +256,6 @@ export const VIDEO: FlutterTech[] = [
     name: "speechify",
     img: "https://iili.io/3u0UO3g.webp",
     link: "https://speechify.com/ai-voice-generator/?srsltid=AfmBOoq93L5EzF0bphoRdYEkejxgnUsQnUyYL1dYHs57z212IdHVXGHv"
-  }, {
-    id: "use",
-    name: "myedit.online",
-    img: "https://iili.io/3u1MM79.webp",
-    link: "https://myedit.online/es/photo-editor/ai-replace?utm_source=blog_esp&utm_medium=referral&utm_campaign=3009-quitar-la-ropa-en-una-foto&affid=2581_-1_1005_3009_quitar-la-ropa-en-una-foto"
   }, {
     id: "use",
     name: "prezi",
@@ -331,7 +355,7 @@ export const VIDEO: FlutterTech[] = [
 
   //   MARK: --- -- -- BANK IMG
   {
-    id: "bacnk-img",
+    id: "back-img",
     name: "pixabay",
     img: "https://iili.io/3u1AD5Q.webp",
     link: "https://pixabay.com/videos/search/video%20effects/"
